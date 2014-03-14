0f242d5706a8efbc802d98a367d86267453c534c
Marius Cobzarenco <marius@reinfer.io>
2014-03-14 17:11:35 +0000

    renamed package to end with _pb2 and fixed dependency on core_pb2

--- setup.py
+++ setup.py
@@ -14,12 +14,12 @@ except ImportError:
           file=sys.stderr)
     sys.exit(1)
 
-PROTO_PREFIX = '/usr/share/proto/reinferio'
+PROTO_PREFIX = '/usr/share/proto'
 
-PKG_NAME = 'pb_saltfish'
+PKG_NAME = 'saltfish_pb2'
 SetupProtoCompiler.pkg_name = PKG_NAME
 SetupProtoCompiler.files = [('saltfish.proto', True)]
-SetupProtoCompiler.deps = [path.join(PROTO_PREFIX, 'core.proto')]
+SetupProtoCompiler.include_dirs = ['/usr/share/proto']
 
 setup(name='reinferio.' + PKG_NAME,
       version='0.1.0',
@@ -30,6 +30,7 @@ setup(name='reinferio.' + PKG_NAME,
       test_suite='test',
       install_requires=[
           'reinferio.proto>=0.1.0',
+          'reinferio.core_pb2>=0.1.0',
           'rpcz>=0.9',
       ],
       cmdclass={
